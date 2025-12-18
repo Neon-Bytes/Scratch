6867cc0d7630ae21c9be0c3d99b2b03e84b3eb0e
Neon-Bytes <nionbytes@gmail.com>
2025-12-18 13:28:49 +0530

    just practising

--- pandas/dataframes in pandas.py
+++ pandas/dataframes in pandas.py
@@ -2,8 +2,17 @@ import pandas as pd
 
 data={"name":["arjun","karna","duryodhan","bishma-pitama"],
       "age":[34,36,53,102]}
+index=["Emp1","Emp2","Emp3","Emp4"]
+df=pd.DataFrame(data,index=index)
+print(df.loc["Emp3"])    #getting location through made up index
 
-df=pd.DataFrame(data)
-
+df["job"]=["Archery","Warrior","Cyclist","Footballer"]     #adding a column
 print(df)
-print(df.loc[2])
+
+#adding a row
+
+new_row=pd.DataFrame([{"name":"Abhimanyu","age":16,"job":"Greatest"},
+                      {"name":"Dronacharya","age":89,"job":"Teacher"}],
+                      index=["Emp4","emp5"])
+df=pd.concat([df,new_row])
+print(df)
